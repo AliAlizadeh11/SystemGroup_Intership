5f4d43003e0c1b2e839e281ec63587b2141b4227
Ali Alizadeh <87001358+AliAlizadeh11@users.noreply.github.com>
2024-10-11 13:30:09 +0330

    Add files via upload
    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MyCollection : IEnumerable<int>
+{
+    private List<int> _items = new List<int>();
+
+    public void Add(int item)
+    {
+        _items.Add(item);
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        return _items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        MyCollection collection = new MyCollection();
+        collection.Add(1);
+        collection.Add(2);
+        collection.Add(3);
+
+        foreach (int item in collection)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
